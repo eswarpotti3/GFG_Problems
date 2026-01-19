0d2b6f48977be76f6f426f0cefa2798f2e5530ae
Eswar Potti <potti.eswar3@gmail.com>
2026-01-19 19:48:20 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Remove K Digits/remove-k-digits.py
@@ -0,0 +1,17 @@
+class Solution:
+    def removeKdig(self, s, k):
+        
+        st = []
+
+        for c in s:
+            while st and k > 0 and st[-1] > c:
+                st.pop()
+                k -= 1
+            st.append(c)
+
+        while k > 0 and st:
+            st.pop()
+            k -= 1
+
+        res = "".join(st).lstrip("0")
+        return res if res else "0"
